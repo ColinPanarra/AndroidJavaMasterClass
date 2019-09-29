34a226d545114be1447818ac83483a7483be226b
Colin Panarra <colinpanarra@gmail.com>
2019-09-29 10:50:19 -0400

    compelted calculator app

--- Desktop/Code/Udemy_android/Calculator/app/src/main/java/com/example/calculator/MainActivity.java
+++ Desktop/Code/Udemy_android/Calculator/app/src/main/java/com/example/calculator/MainActivity.java
@@ -34,23 +34,26 @@ public class MainActivity extends AppCompatActivity {
         newNumber = findViewById(R.id.newNumber);
         displayOperation = findViewById(R.id.operation);
 
-        Button button0 =  findViewById(R.id.button0);
-        Button button1 =  findViewById(R.id.button1);
-        Button button2 =  findViewById(R.id.button2);
-        Button button3 =  findViewById(R.id.button3);
-        Button button4 =  findViewById(R.id.button4);
-        Button button5 =  findViewById(R.id.button5);
-        Button button6 =  findViewById(R.id.button6);
-        Button button7 =  findViewById(R.id.button7);
-        Button button8 =  findViewById(R.id.button8);
-        Button button9 =  findViewById(R.id.button9);
-        Button buttonDot =  findViewById(R.id.buttonDot);
+        Button button0 = findViewById(R.id.button0);
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+        Button button3 = findViewById(R.id.button3);
+        Button button4 = findViewById(R.id.button4);
+        Button button5 = findViewById(R.id.button5);
+        Button button6 = findViewById(R.id.button6);
+        Button button7 = findViewById(R.id.button7);
+        Button button8 = findViewById(R.id.button8);
+        Button button9 = findViewById(R.id.button9);
+        Button buttonDot = findViewById(R.id.buttonDot);
 
-        Button buttonEquals =  findViewById(R.id.buttonEqual);
-        Button buttonDivide =  findViewById(R.id.buttonDivide);
-        Button buttonMultiply =  findViewById(R.id.buttonMult);
-        Button buttonMinus =  findViewById(R.id.buttonMinus);
-        Button buttonPlus =  findViewById(R.id.buttonPlus);
+        Button buttonNeg = findViewById(R.id.buttonNeg);
+        Button buttonClr = findViewById(R.id.buttonClr);
+
+        Button buttonEquals = findViewById(R.id.buttonEqual);
+        Button buttonDivide = findViewById(R.id.buttonDivide);
+        Button buttonMultiply = findViewById(R.id.buttonMult);
+        Button buttonMinus = findViewById(R.id.buttonMinus);
+        Button buttonPlus = findViewById(R.id.buttonPlus);
 
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
@@ -72,12 +75,14 @@ public class MainActivity extends AppCompatActivity {
         button9.setOnClickListener(listener);
         buttonDot.setOnClickListener(listener);
 
+
         View.OnClickListener opListener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Button b = (Button) view;
                 String op = b.getText().toString();
                 String value = newNumber.getText().toString();
+
 
                 try {
                     Double doubleValue = Double.valueOf(value);
@@ -89,6 +94,8 @@ public class MainActivity extends AppCompatActivity {
                 pendingOperation = op;
                 displayOperation.setText(pendingOperation);
             }
+
+
         };
         buttonEquals.setOnClickListener(opListener);
         buttonDivide.setOnClickListener(opListener);
@@ -97,14 +104,42 @@ public class MainActivity extends AppCompatActivity {
         buttonPlus.setOnClickListener(opListener);
 
 
+        View.OnClickListener negListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Button b = (Button) view;
+                //String neg = b.getText().toString();
+                if (newNumber.getText().toString().length() == 0) {
+                    newNumber.setText("-");
+                    return;
+                }
+                if (newNumber.getText().toString().charAt(0) != '-') {
+                    newNumber.setText('-' + newNumber.getText().toString());
+                } else {
+                    newNumber.setText(newNumber.getText().toString().substring(1));
+                }
+
+            }
+        };
+
+        buttonNeg.setOnClickListener(negListener);
+
+
+        View.OnClickListener clrListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                result.setText("");
+                operand1 = null;
+            }
+        };
+        buttonClr.setOnClickListener(clrListener);
+
     }
-
-
     @Override
-    protected void onSaveInstanceState(Bundle outState){
+    protected void onSaveInstanceState(Bundle outState) {
         outState.putString(OPERAND_CONTENTS, displayOperation.getText().toString());
-        if(operand1!=null){
-            outState.putDouble(STATE_OPERAND1,operand1);
+        if (operand1 != null) {
+            outState.putDouble(STATE_OPERAND1, operand1);
         }
         super.onSaveInstanceState(outState);
 
